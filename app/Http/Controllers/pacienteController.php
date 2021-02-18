f9df0d3f479bb7e6bc5f7c2c731a426a28e14141
Uriel_Aguilar_Ortega <al221711149@gmail.com>
2021-02-18 12:27:34 -0600

    Cambios en catalogo de pacientes

--- app/Http/Controllers/pacienteController.php
+++ app/Http/Controllers/pacienteController.php
@@ -37,7 +37,7 @@ class pacienteController extends Controller
             'estado' => 'required|regex:/^[A-Z][a-z,A-Z, ,á,é,í,ó,ú,ñ]*$/',
             'municipio' => 'required|regex:/^[A-Z][a-z,A-Z, ,á,é,í,ó,ú,ñ]*$/',
             'cp' => 'required|regex:/^[0-9]{5}$/',
-            'calle' => 'required|regex:/^[A-Z][0-9,a-z,A-Z, ,á,é,í,ó,ú,ñ]*$/',
+            'calle' => 'required|regex:/^[0-9,a-z,A-Z, ,á,é,í,ó,ú,ñ]*$/',
             'numero' => 'required|regex:/^[0-9,a-z,A-Z, ,á,é,í,ó,ú,ñ]*$/',
             'email_pac' => 'required|email',
             'pass_pac' => 'required|regex:/^[A-Z,a-z,0-9,á,é,í,ó,ú,ñ]*$/',
